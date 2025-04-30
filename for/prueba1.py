1bf48c7fb72218b946742aad653b6a842c8eec52
Naza2006 <nazario@uamv.edu.ni>
2025-04-30 13:09:54 -0600

    actualizado

--- for/prueba1.py
+++
@@ -1,6 +0,0 @@
-primer_numero =int(input(f"Introduzca un numero \n-> "))
-segundo_numero = int(input(f"Introduzca un segundo numero \n-> "))
-tercer_numero = int(input(f"Introduzca un tercer numero \n-> "))
-
-if primer_numero == segundo_numero and segundo_numero == tercer_numero:
-    print("Son iguales")
